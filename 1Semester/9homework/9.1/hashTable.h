ea3cb09eda9e611d92f633f6ac675af552883ae5
appoliana <polinapikkio@gmail.com>
2018-01-12 08:16:24 +0300

    hashTable is ready

---
+++ 1Semester/9homework/9.1/hashTable.h
@@ -0,0 +1,26 @@
+#pragma once;
+#include "list.h";
+#include <string>;
+
+struct HashTable;
+
+// создание хэш-таблицы
+HashTable *createHashTable(int size);
+
+// проверка на существование элемента 
+bool exist(std::string value, HashTable *table);
+
+// удаление
+void deleteHashTable(HashTable *table);
+
+// добавление в хэш-таблицу
+void addToHashTable(HashTable *table, std::string value);
+
+// печать
+void printHashTable(HashTable *table);
+
+// iувеличение счетчика
+void counterOfWods(std::string key, List *bucket);
+
+// значение хэш-функции
+int HashFunction(ElementType value);
