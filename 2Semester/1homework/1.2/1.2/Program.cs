eaf0c0dc442595697724aad603f9040cf50b1f9a
appoliana <polinapikkio@gmail.com>
2018-02-16 12:25:10 +0300

    1.1

---
+++ 2Semester/1homework/1.2/1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    class Program
+    {
+        public static int fibonacciNumbers(int n)
+        {
+            if (n == 1 || n == 2)
+                return 1;
+            if (n > 2)
+                return fibonacciNumbers(n - 1) + fibonacciNumbers(n - 2);
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int value = 0;
+            Console.Write("Введите число, для которого хотите посчитать число Фибоначчи: ");
+            value = Convert.ToInt32(Console.ReadLine());
+            int result = fibonacciNumbers(value);
+            Console.Write("Резултьат: " + result);
+            int t = new int();
+        }
+    }
+}
