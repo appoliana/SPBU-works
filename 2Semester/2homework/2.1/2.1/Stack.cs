50a80f8d687f5408e97853d3850fce35abc7b397
appoliana <polinapikkio@gmail.com>
2018-03-03 22:09:07 +0300

    2.1 and 2.2

---
+++ 2Semester/2homework/2.1/2.1/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Testing
+{
+    public class Stack
+    {
+        private class StackElement 
+        {
+            private int value;
+            public int Value
+            {
+                get => this.value;
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new Exception("Field is negative");
+                    }
+                }
+            }
+            public StackElement Next { get; set; }  
+        }
+
+        private StackElement head = null;
+        public void Push(int value)
+        {
+            this.head = new StackElement
+            {
+                Next = this.head,
+                Value = value
+            };
+        }
+
+        public int Pop()
+        {
+            if (this.Empty)
+            {
+                return -1;
+            }
+            var value = this.head.Value;
+            this.head = this.head.Next;
+            return value;
+        }
+        public bool Empty => this.head == null;  
+    }      
+}
---
+++ 2Semester/2homework/2.1/Stack.Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stack.Tests
+{
+    [TestClass]  
+    public class UnitTest1
+    {
+        private Testing.Stack stack;
+
+        [TestInitialize]  
+        public void Init()
+        {
+            this.stack = new Testing.Stack();
+        }
+
+        [TestMethod]
+        public void PushAndPopShallReturnSameValue()
+        {     
+            stack.Push(1);
+            var result = stack.Pop();
+            Assert.AreEqual(1, result); 
+        }
+
+        [TestMethod]
+        public void PopFromEmptyStackShallNotCrash()
+        {
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void ConsequentPusheAndPopsShallWork()
+        {
+            stack.Push(1);
+            stack.Push(2);  
+            stack.Push(3);
+
+            Assert.AreEqual(3, stack.Pop()); 
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Pop());
+
+        }
+
+        [TestMethod]
+        public void PushZeroesShallWork()
+        {
+            for (int i = 0; i < 10; ++i) 
+            {
+                stack.Push(0);
+            }
+            for (int i = 0; i < 10; ++i)
+            {
+                Assert.AreEqual(0, stack.Pop());
+            }
+        }
+
+        [TestMethod]
+        public void PushStringShallNotWork()
+        {
+            stack.Push("string");
+        }
+    }
+}
+
