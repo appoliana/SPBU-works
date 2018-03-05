a4d02e6ec641e5ed609a2a05905779465e3e10fd
appoliana <polinapikkio@gmail.com>
2018-03-05 23:15:16 +0300

    remove .vs

---
+++ .Trash/2.3/HashTable.Test/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTable.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private HashTableClass.HashTable hashTable;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.hashTable = new HashTableClass.HashTable();
+        }
+
+        [TestMethod]
+        public void AddElementToHashTable()
+        {
+            hashTable.AddElement("try");
+            hashTable.AddElement("program");
+            Assert.AreEqual(hashTable.Excist("try"), true);
+        }
+
+        [TestMethod]
+        public void DeleteElementFromHashTable()
+        {
+            hashTable.AddElement("try");
+            hashTable.AddElement("program");
+            hashTable.DeleteElement("program");
+            Assert.AreEqual(hashTable.Excist("program"), false);
+        }
+
+        [TestMethod]
+        public void ExcistElementFromHashTable()
+        {
+            hashTable.AddElement("try");
+            hashTable.AddElement("program");
+            Assert.AreEqual(hashTable.Excist("program"), true);
+            Assert.AreEqual(hashTable.Excist("LoveSweta"), false);
+        }
+    }
+}
