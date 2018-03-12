d691fd4462be5414a88a30480e049ebdaf064a8b
appoliana <polinapikkio@gmail.com>
2018-03-12 17:59:13 +0300

    2homework

---
+++ 2Semester/2homework/2.4/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CalculatorClass
+{
+    public class Calculator
+    {
+        public void CountExcpression(char symbol)
+        {
+            int secondNumber = stack.Pop();
+            int firstNumber = stack.Pop();
+            switch (symbol)
+            {
+                case '+':
+                    {
+                        stack.Push(firstNumber + secondNumber);
+                        break;
+                    }
+                case '-':
+                    {
+                        stack.Push(firstNumber - secondNumber);
+                        break;
+                    }
+                case '/':
+                    {
+                        stack.Push(firstNumber / secondNumber);
+                        break;
+                    }
+                case '*':
+                    {
+                        stack.Push(firstNumber * secondNumber);
+                        break;
+                    }
+            }
+        }
+    }
+}
