d82e838fcbe522d91184320302b92fdc0e6e8a02
appoliana <polinapikkio@gmail.com>
2018-03-12 18:11:04 +0300

    1homework

---
+++ 2Semester/1homework/1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1._2
+{
+    class Program
+    {
+        public static int FibonacciNumbers(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            if (n > 2)
+            {
+                return FibonacciNumbers(n - 1) + FibonacciNumbers(n - 2);
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int value = 0;
+            Console.Write("Введите число, для которого хотите посчитать число Фибоначчи: ");
+            value = Convert.ToInt32(Console.ReadLine());
+            int result = FibonacciNumbers(value);
+            Console.Write("Результат: " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2Semester/1homework/1.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1._3
+{
+    class Program
+    {
+        public static void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; ++i)
+            {
+                for (int j = 0; j < array.Length - 1; ++j)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int dop = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = dop;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int length = 0;
+            Console.Write("Введите количеcтво элементов массива: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[length];
+            Console.Write("Введите элементы массива: ");
+            for (int i = 0; i < length; ++i)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Sort(array);
+            Console.Write("Oтсортированный массив: ");
+            for (int i = 0; i < length; ++i)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
