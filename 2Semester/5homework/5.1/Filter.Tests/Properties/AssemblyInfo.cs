f135e90c123d8d661ba74c7d6b0d6d940c951fd7
appoliana <polinapikkio@gmail.com>
2018-03-16 12:42:21 +0300

    new 5.1

---
+++ 2Semester/5homework/5.1/Filter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Filter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Filter.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25865456-0d17-4ec4-957b-8d9705de68d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 2Semester/5homework/5.1/Fold.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fold
+{
+    class FoldFunction
+    {
+        public static List<int> Fold(List<int> list, int begginingValue, Func<int, int> func)
+        {
+            var result = 0;
+            for (int i = 0; i < list.Count; ++i)
+            {
+                result = func(begginingValue, list[i]);
+            }
+            return result;
+        }
+    }
+}
