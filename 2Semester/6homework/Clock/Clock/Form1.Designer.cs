36117915b0f9e9115ab4911b886f739d102ab4c1
appoliana <polinapikkio@gmail.com>
2018-03-25 21:41:05 +0300

    delete .vs and obj

---
+++ 2Semester/6homework/Clock/Clock/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(105, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 86);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(148, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 47);
+            this.label2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(476, 243);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 2Semester/6homework/Clock/Clock/HMS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Clock
+{
+    class HMS
+    {
+        public HMS (DateTime dateTime)
+        {
+            if (dateTime.Hour < 10)
+            {
+                Hour = $"0{dateTime.Hour}";
+            }
+            else
+            {
+                Hour = dateTime.Hour.ToString();
+            }
+
+            if (dateTime.Minute < 10)
+            {
+                Minute = $"0{dateTime.Minute}";
+            }
+            else
+            {
+                Minute = dateTime.Minute.ToString();
+            }
+
+            if (dateTime.Second < 10)
+            {
+                Secound = $"0{dateTime.Second}";
+            }
+            else
+            {
+                Secound = dateTime.Second.ToString();
+            }
+        }
+        public string Hour { get; }
+        public string Minute { get; }
+        public string Secound { get; }
+    }
+}
