70d2330a0bed36cf8f6dec9262c13561a0c7c296
appoliana <polinapikkio@gmail.com>
2018-05-11 11:19:59 +0300

    new

---
+++ 2Semester/test/Progress/Progress/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Progress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Start = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.Close = new System.Windows.Forms.Button();
+            this.TimewAfterBar = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(191, 25);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(84, 78);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Click me!";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(58, 145);
+            this.progressBar.Maximum = 500;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(371, 48);
+            this.progressBar.TabIndex = 1;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Interval = 10;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.Color.Red;
+            this.Close.ForeColor = System.Drawing.Color.Black;
+            this.Close.Location = new System.Drawing.Point(191, 25);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(84, 78);
+            this.Close.TabIndex = 2;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = false;
+            this.Close.Visible = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // TimewAfterBar
+            // 
+            this.TimewAfterBar.Interval = 2000;
+            this.TimewAfterBar.Tick += new System.EventHandler(this.TimerAfterBar_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 231);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.Start);
+            this.MaximumSize = new System.Drawing.Size(508, 270);
+            this.MinimumSize = new System.Drawing.Size(508, 270);
+            this.Name = "Form1";
+            this.Text = "ProgressBar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Timer TimewAfterBar;
+    }
+}
+
