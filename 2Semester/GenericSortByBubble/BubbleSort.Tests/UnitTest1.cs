8c3733b968b7084086e15014ef999d53019b9ba5
appoliana <polinapikkio@gmail.com>
2018-05-22 12:45:37 +0300

    generic sort

---
+++ 2Semester/GenericSortByBubble/BubbleSort.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BubbleSort.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SortIntArray()
+        {
+            int size = 10;
+            int[] array = new int[size];
+            for (int i = 9; i >= 0; --i)
+            {
+                array[i] = i;
+            }
+
+            Comparer<int> comparer = Comparer<int>.Default;
+            BubbleSort<int>(array, comparer);
+            Assert.AreEqual(array[0], 0);
+        }
+
+        [TestMethod]
+        public void SortStringArray()
+        {
+            int size = 3;
+            string[] array = new string[size];
+            array[0] = "a";
+            array[1] = "c";
+            array[2] = "b";
+
+            Comparer<string> comparer = Comparer<string>.Default;
+            BubbleSort<string>(array, comparer);
+
+            Assert.AreEqual(array[1], "b");
+        }
+    }
+}
---
+++ 2Semester/GenericSortByBubble/GenericSortByBubble/Sort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBubbleSort
+{
+    public class Sort<T> 
+    {
+        static T [] BubbleSort(T [] array, Comparer<T> comparer)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (comparer.Compare(array[i], array[j]) > 0)
+                    {
+                        T temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
