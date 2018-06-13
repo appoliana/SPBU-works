6e99d6e159b17ff0c5a10d9d969b2912f63c39e8
appoliana <polinapikkio@gmail.com>
2018-06-13 05:07:52 +0300

    isReadygit add 2.1 ))

---
+++ 2Semester/2homework/2.1/2.1/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StackRealization
+{
+    public class Stack
+    {
+        private class StackElement
+        {
+            public int Value { get; set; }
+            public StackElement Next { get; set; }
+        }
+
+            private StackElement head = null;
+        public void Push(int value)
+        {
+            this.head = new StackElement
+            {
+                Next = this.head,
+                Value = value
+            };
+        }
+
+        public int Pop()
+        {
+            if (this.Empty)
+            {
+                return -1;
+            }
+            var value = this.head.Value;
+            this.head = this.head.Next;
+            return value;
+        }
+
+        public bool Empty => this.head == null;
+    }
+}
